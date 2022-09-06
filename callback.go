c001d90ab26d62e86743198ca13a9da7940b5ac5
Ali Hashemi <60406325+MR-AliHaashemi@users.noreply.github.com>
2022-09-06 18:26:15 +0430

    add(callback): Callback data structures

---
+++ callback.go
@@ -0,0 +1,21 @@
+package idpay
+
+import "time"
+
+type PostCallback struct {
+	Status       int       `json:"status"`         // Status is Transaction Status
+	TrackID      int       `json:"track_id"`       // TrackIS is IDPay's track id
+	ID           string    `json:"id"`             // ID is Transaction ID which generated on creating the transaction
+	OrderID      string    `json:"order_id"`       // OrderID is the orderID which sent on creating the transaction
+	Amount       int       `json:"amount"`         // Amount is submitted amount on creating the transaction
+	CardNo       string    `json:"card_no"`        // CardNo is payer's card number in the format of 123456******1234
+	HashedCardNo string    `json:"hashed_card_no"` // HashedCardNo is hashed card number with SHA256 algorithm
+	Date         time.Time `json:"date"`           // Date is the transaction payment time
+}
+
+type GetCallback struct {
+	Status  int    `json:"status"`   // Status is Transaction Status
+	TrackID int    `json:"track_id"` // TrackIS is IDPay's track id
+	ID      string `json:"id"`       // ID is Transaction ID which generated on creating the transaction
+	OrderID string `json:"order_id"` // OrderID is the orderID which sent on creating the transaction
+}
