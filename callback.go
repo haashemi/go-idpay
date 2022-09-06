d71b5e04868ccde0f07412e11a2a9d786e5b6a88
Ali Hashemi <60406325+MR-AliHaashemi@users.noreply.github.com>
2022-09-06 19:36:27 +0430

    update(callback): use UnixTime instead of time.Time

--- callback.go
+++ callback.go
@@ -1,16 +1,14 @@
 package idpay
 
-import "time"
-
 type PostCallback struct {
-	Status       Number    `json:"status"`         // Status is Transaction Status
-	TrackID      Number    `json:"track_id"`       // TrackIS is IDPay's track id
-	ID           string    `json:"id"`             // ID is Transaction ID which generated on creating the transaction
-	OrderID      string    `json:"order_id"`       // OrderID is the orderID which sent on creating the transaction
-	Amount       Number    `json:"amount"`         // Amount is submitted amount on creating the transaction
-	CardNo       string    `json:"card_no"`        // CardNo is payer's card number in the format of 123456******1234
-	HashedCardNo string    `json:"hashed_card_no"` // HashedCardNo is hashed card number with SHA256 algorithm
-	Date         time.Time `json:"date"`           // Date is the transaction payment time
+	Status       Number   `json:"status"`         // Status is Transaction Status
+	TrackID      Number   `json:"track_id"`       // TrackIS is IDPay's track id
+	ID           string   `json:"id"`             // ID is Transaction ID which generated on creating the transaction
+	OrderID      string   `json:"order_id"`       // OrderID is the orderID which sent on creating the transaction
+	Amount       Number   `json:"amount"`         // Amount is submitted amount on creating the transaction
+	CardNo       string   `json:"card_no"`        // CardNo is payer's card number in the format of 123456******1234
+	HashedCardNo string   `json:"hashed_card_no"` // HashedCardNo is hashed card number with SHA256 algorithm
+	Date         UnixTime `json:"date"`           // Date is the transaction payment time
 }
 
 type GetCallback struct {
