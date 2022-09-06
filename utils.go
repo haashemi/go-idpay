e9ab75825ddbf763c6459c762223fd34450931e6
er-azh <80633916+er-azh@users.noreply.github.com>
2022-09-06 19:27:23 +0430

    add(utils): add a type for parsing unix timestamps

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package idpay
 
 import (
 	"strconv"
+	"time"
 )
 
 type Number int64
@@ -25,4 +26,27 @@ func (c *Number) UnmarshalJSON(b []byte) error {
 
 func (c Number) Int64() int64 {
 	return int64(c)
+}
+
+type UnixTime time.Time
+
+func (c *UnixTime) MarshalJSON() ([]byte, error) {
+	return []byte(strconv.Quote(strconv.FormatInt(time.Time(*c).Unix(), 10))), nil
+}
+
+func (c *UnixTime) UnmarshalJSON(b []byte) error {
+	s, err := strconv.Unquote(string(b))
+	if err != nil {
+		s = string(b)
+	}
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return err
+	}
+	*c = UnixTime(time.Unix(i, 0))
+	return nil
+}
+
+func (c UnixTime) AsTime() time.Time {
+	return time.Time(c)
 }
