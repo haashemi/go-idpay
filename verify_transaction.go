bea2e95e514456123d12d1a98bffac81df269a08
Ali Hashemi <60406325+MR-AliHaashemi@users.noreply.github.com>
2022-09-06 19:36:17 +0430

    update(verify_transaction): use UnixTime instead of time.Time

--- verify_transaction.go
+++ verify_transaction.go
@@ -4,25 +4,24 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
-	"time"
 )
 
 type VerifyTransaction struct {
-	Status  Number    `json:"status"`   // Status is Transaction Status
-	TrackID Number    `json:"track_id"` // TrackIS is IDPay's track id
-	ID      string    `json:"id"`       // ID is Transaction ID which generated on creating the transaction
-	OrderID string    `json:"order_id"` // OrderID is the orderID which sent on creating the transaction
-	Amount  Number    `json:"amount"`   // Amount is submitted amount on creating the transaction
-	Date    time.Time `json:"date"`     // Date is the transaction creation time
+	Status  Number   `json:"status"`   // Status is Transaction Status
+	TrackID Number   `json:"track_id"` // TrackIS is IDPay's track id
+	ID      string   `json:"id"`       // ID is Transaction ID which generated on creating the transaction
+	OrderID string   `json:"order_id"` // OrderID is the orderID which sent on creating the transaction
+	Amount  Number   `json:"amount"`   // Amount is submitted amount on creating the transaction
+	Date    UnixTime `json:"date"`     // Date is the transaction creation time
 	Payment struct {
-		TrackID      string    `json:"track_id"`       // TrackIS is IDPay's track id
-		Amount       Number    `json:"amount"`         // Amount is the amount payable
-		CardNo       string    `json:"card_no"`        // CardNo is payer's card number in the format of 123456******1234
-		HashedCardNo string    `json:"hashed_card_no"` // HashedCardNo is hashed card number with SHA256 algorithm
-		Date         time.Time `json:"date"`           // Date is the transaction payment time
+		TrackID      string   `json:"track_id"`       // TrackIS is IDPay's track id
+		Amount       Number   `json:"amount"`         // Amount is the amount payable
+		CardNo       string   `json:"card_no"`        // CardNo is payer's card number in the format of 123456******1234
+		HashedCardNo string   `json:"hashed_card_no"` // HashedCardNo is hashed card number with SHA256 algorithm
+		Date         UnixTime `json:"date"`           // Date is the transaction payment time
 	} `json:"payment"` // Payment contains the transaction's payment info
 	Verify struct {
-		Date time.Time `json:"date"` // Date is the transaction verification time
+		Date UnixTime `json:"date"` // Date is the transaction verification time
 	} `json:"verify"` // Verify contains payment verification info
 }
 
